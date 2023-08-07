2490de34ada43465b9415920cfd365579f5114b5
Moorthi <118035537+Moorthi45@users.noreply.github.com>
2023-08-07 20:39:45 +0530

    Delete scr.js

--- day-15 task/form-task/scr.js
+++
@@ -1,7 +0,0 @@
-let foods=['chicken','briyany'];
-foods.forEach((val)=>{
-  const opt= document.createElement('option')
-  opt.textContent= val
-  opt.value=val
-  document.getElementById('food').appendChild(opt)
-})
